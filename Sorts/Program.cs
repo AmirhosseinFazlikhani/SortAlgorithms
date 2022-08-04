510409e7dec85b82eea7b9daafcf2b88cf727993
amirhossein fazlikhani <afazlikhani98@gmail.com>
2022-08-04 16:17:04 +0430

    Refactor benchmarking

--- Sorts/Program.cs
+++ Sorts/Program.cs
@@ -3,25 +3,20 @@ using Sorts;
 
 var arrays = CreateArrays(4);
 
-var watch = Stopwatch.StartNew();
-_ = arrays[0].OrderBy(i => i).ToArray();
-watch.Stop();
-Console.WriteLine($"Linq: {watch.Elapsed}");
+var watch = new Stopwatch();
 
-watch.Restart();
-_ = arrays[1].HeapSort(i => i);
-watch.Stop();
-Console.WriteLine($"Heap: {watch.Elapsed}");
+Benchmark("Linq", watch, () => _ = arrays[0].OrderBy(i => i).ToArray());
+Benchmark("Heap", watch, () => _ = arrays[1].HeapSort(i => i));
+Benchmark("Shell", watch, () => _ = arrays[2].ShellSort(i => i));
+Benchmark("Insertion", watch, () => _ = arrays[3].InsertionSort(i => i));
 
-watch.Restart();
-_ = arrays[2].ShellSort(i => i);
-watch.Stop();
-Console.WriteLine($"Shell: {watch.Elapsed}");
-
-watch.Restart();
-_ = arrays[3].InsertionSort(i => i);
-watch.Stop();
-Console.WriteLine($"Insertion: {watch.Elapsed}");
+void Benchmark(string name, Stopwatch watch, Action action)
+{
+    watch.Restart();
+    action();
+    watch.Stop();
+    Console.WriteLine($"{name}: {watch.Elapsed}");
+}
 
 List<int[]> CreateArrays(int count)
 {
